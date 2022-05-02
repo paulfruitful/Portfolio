d6fa3dee3778e83271d98a82a78a9dc7d6af1204
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2022-05-02 14:40:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.querySelector(".navbar-link");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function see(){
+    let show=document.getElementById('show');
+    let see=document.getElementById('see')
+    let k=document.getElementById('knowledge')
+    let i=document.getElementById('skill')
+    show.style.display="block";
+    if(show.style.display=='block'){
+      see.style.display="none";
+      i.style.marginTop="150px"
+    }
+  }
